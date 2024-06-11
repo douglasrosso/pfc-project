5c70e7f7bdbf450d221cb45b4dc7b302e4bd96e7
Anderson <andersonrw16@hotmail.com>
2024-06-11 20:34:20 -0300

    wip

--- src/app/components/Appbar.js
+++ src/app/components/Appbar.js
@@ -2,8 +2,25 @@
 
 import { useContext } from "react";
 import { AuthContext } from "../contexts/AuthContext";
+import axios from "axios";
+import { useRouter } from "next/navigation";
 
 export function Appbar() {
+  const router = useRouter();
+  const { setIsAuthenticated } = useContext(AuthContext);
+
+  async function handleExitClicked() {
+    const response = await axios.get("/api/logout");
+    
+    if (response.data.success) {
+      setIsAuthenticated(false);
+      router.replace("/");
+    }
+  }
   const { isAuthenticated } = useContext(AuthContext);
-  return isAuthenticated ? <div>Appbar - em construção</div> : null;
+  return isAuthenticated ? (
+    <div>
+      Appbar - em construção <button onClick={handleExitClicked}>Sair</button>
+    </div>
+  ) : null;
 }
